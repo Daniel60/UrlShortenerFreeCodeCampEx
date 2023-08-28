3da6853c2f4f4587c206c8d7364902dcd38b0912
Daniel60 <39488923+Daniel60@users.noreply.github.com>
2023-08-28 12:08:01 -0300

    completed

--- index.js
+++ index.js
@@ -1,24 +1,55 @@
-require('dotenv').config();
-const express = require('express');
-const cors = require('cors');
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
 const app = express();
+const dns = require("dns");
+const bodyParser = require("body-parser");
 
+const listUrlSaved = {};
 // Basic Configuration
 const port = process.env.PORT || 3000;
 
 app.use(cors());
 
-app.use('/public', express.static(`${process.cwd()}/public`));
+app.use("/public", express.static(`${process.cwd()}/public`));
 
-app.get('/', function(req, res) {
-  res.sendFile(process.cwd() + '/views/index.html');
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.get("/", function (req, res) {
+  res.sendFile(process.cwd() + "/views/index.html");
 });
 
 // Your first API endpoint
-app.get('/api/hello', function(req, res) {
-  res.json({ greeting: 'hello API' });
+app.post("/api/shorturl", function (req, res) {
+  const url = new URL(req.body.url);
+  resp_json = {};
+  dns.lookup(url.hostname, function (err, address) {
+    if (err) {
+      resp_json = { error: "invalid url" };
+      res.json(resp_json);
+    } else {
+      const urlId = Object.keys(listUrlSaved).length + 1;
+      listUrlSaved[urlId] = url;
+      resp_json = { original_url: url, short_url: urlId };
+      res.json(resp_json);
+    }
+  });
 });
 
-app.listen(port, function() {
+app.get("/api/shorturl/:short_url", function (req, res) {
+  const short_url = req.params.short_url;
+  const urlProtocol = listUrlSaved[short_url];
+  resp_json = {};
+  if (urlProtocol) {
+    console.log("redirect", urlProtocol);
+    res.redirect(urlProtocol);
+  } else {
+    resp_json = { error: "invalid url" };
+    res.json(resp_json);
+  }
+});
+
+app.listen(port, function () {
   console.log(`Listening on port ${port}`);
 });
